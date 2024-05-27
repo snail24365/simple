244ab141da7477572507438a4dfca8d5ba596677
snail24365 <snail24365@gmail.com>
2024-05-27 23:56:19 +0900

    init

--- src/ST.h
+++ src/ST.h
@@ -9,9 +9,9 @@ SYMBOL TABLE RECORD
 -------------------------------------------------------------------------*/
 struct symrec
 {
-char *name; /* name of symbol */
-int offset; /* data offset */
-struct symrec *next; /* link field */
+  char *name;          /* name of symbol */
+  int offset;          /* data offset */
+  struct symrec *next; /* link field */
 };
 typedef struct symrec symrec;
 /*-------------------------------------------------------------------------
@@ -26,25 +26,25 @@ symrec *sym_table = (symrec *)0; /* The pointer to the Symbol Table */
 /*========================================================================
 Operations: Putsym, Getsym
 ========================================================================*/
-symrec * putsym (char *sym_name)
+symrec *putsym(char *sym_name)
 {
-symrec *ptr;
-ptr = (symrec *) malloc (sizeof(symrec));
-ptr->name = (char *) malloc (strlen(sym_name)+1);
-strcpy (ptr->name,sym_name);
-ptr->offset = data_location();
-ptr->next = (struct symrec *)sym_table;
-sym_table = ptr;
-return ptr;
+  symrec *ptr;
+  ptr = (symrec *)malloc(sizeof(symrec));
+  ptr->name = (char *)malloc(strlen(sym_name) + 1);
+  strcpy(ptr->name, sym_name);
+  ptr->offset = data_location();
+  ptr->next = (struct symrec *)sym_table;
+  sym_table = ptr;
+  return ptr;
 }
-symrec * getsym (char *sym_name)
+symrec *getsym(char *sym_name)
 {
-symrec *ptr;
-for ( ptr = sym_table;
-ptr != (symrec *) 0;
-ptr = (symrec *)ptr->next )
-if (strcmp (ptr->name,sym_name) == 0)
-return ptr;
-return 0;
+  symrec *ptr;
+  for (ptr = sym_table;
+       ptr != (symrec *)0;
+       ptr = (symrec *)ptr->next)
+    if (strcmp(ptr->name, sym_name) == 0)
+      return ptr;
+  return 0;
 }
 /************************** End Symbol Table **************************/
